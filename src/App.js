102e4937b901bae5a003772e32d8b5e09ed5489d
WebWalterWrite <webwalterwhrite@gmail.com>
2019-08-07 16:47:53 +0200

    :art: add Register component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Register from './components/form/Register';
+import ea from './assets/ea-logo.png';
 
-function App() {
+ const App = () => {
+
   return (
-    <div className="App">
+    <>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>DYNAMIC-FORM by <img src={ea} alt="" width="50"/></h1>
       </header>
-    </div>
+      <main className="App-main">
+        <Register/>
+      </main>
+    </>
   );
 }
 
